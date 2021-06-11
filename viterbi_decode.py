8aa6ad4d45d5dfd7423b6add270af04fb3283327
jf <jf@MacBook-Air-dk.local>
2021-06-11 17:20:15 +0800

    update

--- viterbi_decode.py
+++ viterbi_decode.py
@@ -43,66 +43,52 @@ import numpy as np
 
 
 
-def viterbi_decode(transition_matrix, emission_matrix, pi, N, observed_sequence, eps):
+def viterbi_decode(transition_matrix, emission_matrix, pi, observed_sequence):
+    # transition_matrix, emission_matrix, pi是隐马尔科夫模型的三要素
+    num_status = len(transition_matrix) # 状态的种类个数
+    seq_len = len(observed_sequence) # T
 
-    eps = eps
-
-    if observed_sequence.ndim == 1:
-        observed_sequence = observed_sequence.reshape(1, -1)
-
-    # number of observations in each sequence 
-    T = observed_sequence.shape[1]
-
-    # number of training sequences
-    I = observed_sequence.shape[0]
-    if I != 1:
-        raise ValueError(
-            "Can only decode a single sequence (O.shape[0] must be 1)")
-
-    # initialize the viterbi and back_pointer matrices
-    viterbi = np.zeros((N, T))
-    back_pointer = np.zeros((N, T)).astype(int)
-
-    ot = observed_sequence[0, 0]
-    for s in range(N):
+    all_path_prob = np.zeros((num_status, seq_len))  # (N,T)  用来记录所有的路径概率的log形式
+    back_pointer = np.zeros((num_status, seq_len)).astype(np.int) # 用来记录某个概率最大路径的上一个最优节点
+    # t=0
+    curr_observe = observed_sequence[0]
+    for s in range(num_status):
+        all_path_prob[s, 0] = np.log(pi[s]) + np.log(
+            emission_matrix[s, curr_observe]
+        )
         back_pointer[s, 0] = 0
-        viterbi[s, 0] = np.log(pi[s] + eps) + np.log(emission_matrix[s, ot] + eps)
-
-    # T表示观测序列长度，N表示隐状态的数量，或者说搜索空间的宽度
-    # 时间复杂度为O(T*N*N)
-    for t in range(1, T):
-        ot = observed_sequence[0, t]
-        for s in range(N):
-            seq_probs = [
-                viterbi[s_, t - 1]
-                + np.log(transition_matrix[s_, s] + eps)
-                + np.log(emission_matrix[s, ot] + eps)
-                for s_ in range(N)
+    # t>=1
+    for t in range(1, seq_len):
+        curr_observe = observed_sequence[t]
+        for s in range(num_status):
+            curr_prob_seq = [
+                all_path_prob[s_, t - 1]
+                + np.log(transition_matrix[s_, s])
+                + np.log(emission_matrix[s, curr_observe])
+                for s_ in range(num_status)
             ]
+            all_path_prob[s, t] = np.max(curr_prob_seq)
+            back_pointer[s, t] = np.argmax(curr_prob_seq)
 
-            viterbi[s, t] = np.max(seq_probs)
-            back_pointer[s, t] = np.argmax(seq_probs)
+    best_path_log_prob = np.max(all_path_prob[:, seq_len - 1])
+    best_pointer = np.argmax(all_path_prob[:, seq_len - 1])
+    best_path = [best_pointer]
 
-    best_path_log_prob = viterbi[:, T - 1].max()
-
-    # backtrack through the trellis to get the most likely sequence of
-    # latent states
-    pointer = viterbi[:, T - 1].argmax()
-    best_path = [pointer]
-    for t in reversed(range(1, T)):
-        pointer = back_pointer[pointer, t]
-        best_path.append(pointer)
+    # reverse
+    for t in reversed(range(1, seq_len)):
+        best_pointer = back_pointer[best_pointer, t]
+        best_path.append(back_pointer[best_path[-1], t])
     best_path = best_path[::-1]
+
     return best_path, best_path_log_prob
 
 #test:
-"""
-
-A=np.array([[0.5,0.2,0.3],[0.3,0.5,0.2],[0.2,0.3,0.5]])
-B=np.array([[0.5,0.5],[0.4,0.6],[0.7,0.3]])
-pi=np.array([0.4,0.1,0.4])
-N=3
-o=np.array([1,1,0])
-eps=11e-3
-print(decode(A,B,pi,N,o,eps))
-"""
+# """
+if __name__=="__main__":
+    A=np.array([[0.5,0.2,0.3],[0.3,0.5,0.2],[0.2,0.3,0.5]])
+    B=np.array([[0.5,0.5],[0.4,0.6],[0.7,0.3]])
+    pi=np.array([0.4,0.1,0.4])
+    N=3
+    o=np.array([1,1,0])
+    print(viterbi_decode(A,B,pi,o))
+# """
